b4ecac70b415ecb52cd967d4531293fce7c26ceb
ksikorski1 <axfitey@gmail.com>
2020-03-19 17:38:46 +0100

    listening

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+
+const client = redis.createClient({
+	host: 'redis-server',
+	port: 6379
+});
+
+client.set('counter', 0);
+
+app.get('/', (req, resp) => 
+	{
+	console.log('new request');
+	//process.exit(0);
+	client.get('counter', (err,counter) => {
+		resp.send("Hello from node backend");
+		client.set('counter', parseInt(counter) + 1);
+	});
+});
+
+app.listen(8080, () => {
+	console.log("server listening on port 8080");
+});
