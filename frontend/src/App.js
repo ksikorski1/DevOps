0f7ab17480a476579b1b428341000445f56b891a
ksikorski1 <axfitey@gmail.com>
2020-04-09 13:04:03 +0200

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world without covid
+          Hello world without covid !
         </p>
         <a
           className="App-link"
