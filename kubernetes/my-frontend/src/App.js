2481fcdf7ae274355d078a5b74c403d8cfdf6b69
ksikorski1 <axfitey@gmail.com>
2020-05-07 17:24:17 +0200

    kubernetes frontend init

---
+++ kubernetes/my-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Prime from './components/prime.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Prime />
+      </header>
+    </div>
+  );
+}
+
+export default App;
