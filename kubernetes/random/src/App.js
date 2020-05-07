6f6093d64951daab1766cf88f00b6926380002ff
ksikorski1 <axfitey@gmail.com>
2020-05-07 14:43:38 +0200

    random

---
+++ kubernetes/random/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import randomNumber from './random';
+
+function App() {
+  return (
+    <div className="App">
+      {randomNumber}
+    </div>
+  );
+}
+
+export default App;
